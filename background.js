2a8b3865dabb47a43620ed09a81151c6fbb1436b
Daniel Dawson <dannyldawson@gmail.com>
2020-05-18 22:55:40 -0700

    remove google analytics
    As of now, we are only choosing to embed our code into another page to
    enhance the user's experience.
    
    However, this means it is dubious to try to inject our GA script.
    
    I will attempt to use this again in the future if I decide to move logic
    to a separate window or popup.

--- background.js
+++ background.js
@@ -1,30 +1,33 @@
-const ua = "UA-160250616-2";
-(function (i, s, o, g, r, a, m) {
-  i["GoogleAnalyticsObject"] = r;
-  (i[r] =
-    i[r] ||
-    function () {
-      (i[r].q = i[r].q || []).push(arguments);
-    }),
-    (i[r].l = 1 * new Date());
-  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-})(
-  window,
-  document,
-  "script",
-  "https://www.google-analytics.com/analytics.js",
-  "ga"
-);
-ga("create", "UA-160250616-2", "auto");
-ga("set", "checkProtocolTask", null);
-ga("send", "pageview");
-chrome.runtime.onMessage.addListener(function (req, sender, sendResponse) {
-  if (req.action === "extensionLoaded") {
-    ga("send", "pageview");
-    console.log(ga);
-    console.log("IT WORKED");
-  }
-});
+// const ua = "UA-160250616-2";
+// (function (i, s, o, g, r, a, m) {
+//   i["GoogleAnalyticsObject"] = r;
+//   (i[r] =
+//     i[r] ||
+//     function () {
+//       (i[r].q = i[r].q || []).push(arguments);
+//     }),
+//     (i[r].l = 1 * new Date());
+//   (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+//   a.async = 1;
+//   a.src = g;
+//   m.parentNode.insertBefore(a, m);
+// })(
+//   window,
+//   document,
+//   "script",
+//   "https://www.google-analytics.com/analytics.js",
+//   "ga"
+// );
+// ga("create", "UA-160250616-2", "auto");
+// ga("set", "checkProtocolTask", null);
+// ga("send", "pageview");
+// chrome.runtime.onMessage.addListener(function (req, sender, sendResponse) {
+//   if (req.action === "extensionLoaded") {
+//     ga("send", "pageview");
+//     console.log(ga);
+//     console.log("IT WORKED");
+//   }
+// });
+
+// in manifest
+// "content_security_policy": "script-src 'self' https://www.google-analytics.com; object-src 'self'"
